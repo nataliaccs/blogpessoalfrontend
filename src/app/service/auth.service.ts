f1e548cf8811d34a212e7c8f73212f58b2a702f2
NatáliaCosta <naty1993ccs@gmail.com>
2022-09-05 19:38:19 -0300

    componente início

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { User } from '../model/User';
 import { Observable } from 'rxjs';
 import { UserLogin } from '../model/UserLogin';
+import { environment } from 'src/environments/environment.prod';
 
 
 @Injectable({
@@ -15,11 +16,21 @@ export class AuthService {
   ) { }
 
     login(userLogin: UserLogin): Observable<UserLogin>{
-      return this.http.post<UserLogin>('http://localhost:8080/usuarios/logar',userLogin)
+      return this.http.post<UserLogin>('https://blogpessoalnataliacosta.herokuapp.com/usuarios/logar',userLogin)
     }
 
     cadastrar(user: User): Observable<User>{
-      return this.http.post<User>('http://localhost:8080/usuarios/cadastrar',user)
+      return this.http.post<User>('https://blogpessoalnataliacosta.herokuapp.com/usuarios/cadastrar',user)
+    }
+
+    logado(){
+      let ok: boolean = false
+
+      if (environment.token != ''){
+        ok = true
+      }
+
+      return ok 
     }
 
 }
